10c259d91fcc0ab952c6b0895b0951c732426091
krise3k <krise3k@gmail.com>
2018-10-23 23:36:26 +0200

    Sirupsen/logrus -> sirupsen/logrus

--- models/containers.go
+++ models/containers.go
@@ -1,7 +1,7 @@
 package models
 
 import (
-	"github.com/Sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 	"github.com/fsouza/go-dockerclient"
 	"github.com/krise3k/armada-stats/models/armada"
 	"github.com/krise3k/armada-stats/utils"
